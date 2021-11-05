41531c682d78f5482e2854f24eabd0eb8ba9936e
Adiissep <74800424+Adiissep@users.noreply.github.com>
2021-11-05 10:30:07 +0700

    Add files via upload

---
+++ ProgramOutput.java
@@ -0,0 +1,14 @@
+package Sesi1;
+/**
+ * @author ADVANG4C
+ * Nama     : Adi Sepriyadi
+ * Kelas    : TI20E
+ * NIM      : 20200040093
+ */
+public class ProgramOutput {
+
+    public static void main(String[] args) {
+        System.out.println("Belajar Java\n"+"Sangat Mudah Sekali\n"+"Dan Sangat Menyenangkan");
+    }
+    
+}
