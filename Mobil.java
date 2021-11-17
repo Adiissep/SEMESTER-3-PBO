0a60a9a855cf277b3d828bd8b4338c536a742c37
Adiissep <74800424+Adiissep@users.noreply.github.com>
2021-11-17 15:56:54 +0700

    Add files via upload

---
+++ Mobil.java
@@ -0,0 +1,78 @@
+package java_uts_semester3;
+
+/**
+ * @author AD7
+ * NIM   : 20200040093
+ * NAMA  : ADI SEPRIYADI
+ * KELAS : TI20E
+ */
+public class Mobil {
+String merk,tipe;
+boolean nyala = false;
+String status;
+int porseneling;
+int kecepatan;
+
+Mobil(String merk, String tipe_mobil){
+    this.merk=merk;
+    tipe=tipe_mobil;
+   
+}
+public void tambahKecepatan(int kecepatan){
+    if (kecepatan == 0){
+        System.out.println("Mobil sedang berhenti");
+    }else if(kecepatan > 1 && kecepatan <=50 ){
+        System.out.println("Mobil sedang lambat");
+    }else if(kecepatan >51 && kecepatan <=70){
+        System.out.println("Mobil sedang sedang");
+    }else {
+        System.out.println("Mobil sedang cepat");
+    }
+}
+   
+public void kurangKecepatan(int kecepatan){
+    if (kecepatan == 0){
+        System.out.println("Mobil sedang berhenti");
+    }else if(kecepatan > 1 && kecepatan >=50 ){
+        System.out.println("Mobil sedang lambat");
+    }else if(kecepatan >51 && kecepatan <=70){
+        System.out.println("Mobil sedang sedang");
+    }else {
+        System.out.println("Mobil sedang cepat");
+    }
+}
+public int getKecepatan(){
+    return kecepatan;
+}
+void infoKecepatan(){
+    System.out.println("Merk :"+getStatus());
+    System.out.println("tipe :"+getKecepatan());
+}
+public String getStatus(){
+    return status;
+}
+public String getmerk(){
+    return merk;
+}
+public String gettipe(){
+    return tipe;
+}
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Mobil mobilku = new Mobil("Toyota", "Avanza");
+        System.out.println(mobilku.getStatus());
+        mobilku.infoKecepatan();
+        
+        mobilku.tambahKecepatan(50);
+        System.out.println(mobilku.getStatus());
+        mobilku.infoKecepatan();
+        
+        mobilku.kurangKecepatan(100);
+        System.out.println(mobilku.getStatus());
+        mobilku.infoKecepatan();
+    }
+    
+}
---
+++ Petani1.java
@@ -0,0 +1,55 @@
+package java_uts_semester3;
+import java.util.Arrays;
+/**
+ *
+ * @author AD7
+ * NIM   : 20200040093
+ * NAMA  : ADI SEPRIYADI
+ * KELAS : TI20E
+ * 
+ */
+public class Petani1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static String[] sortItems(String[] items) {
+    for (int i = 0; i < items.length; i++) {
+      String temp = items[i];
+      int position = i;
+      while( position >= 1 && items[position - 1].charAt(0) > temp.charAt(0) ) {
+        items[position] = items[position - 1];
+        position--;
+      }
+      items[position] = temp;
+    }
+
+    return items;
+    }
+    public static void main(String[] args) {
+    String[] boxOfUnorganizedFruits = 
+      {"Apel","Pisang","Anggur","Jambu",
+      "Pepaya","Duku","Durian","Jeruk",
+      "Mangga","Manggis",};
+   
+    int boxIndex = 0;
+    int containerBoxIndex = 0;
+    int boxSize = boxOfUnorganizedFruits.length;
+    String[] boxOfOrganizedFruits = sortItems(boxOfUnorganizedFruits);
+    String[][] boxesOfOrganizedFruits = new String[5][2];
+    String pointer = boxOfOrganizedFruits[0];
+
+    for (int i = 0; i < boxOfOrganizedFruits.length; i++) {
+      if (pointer.charAt(0) != boxOfOrganizedFruits[i].charAt(0)) {
+        pointer = boxOfOrganizedFruits[i];
+        containerBoxIndex++;
+        boxIndex = 0;
+      }
+      if (pointer.charAt(0) == boxOfOrganizedFruits[i].charAt(0)) {
+        boxesOfOrganizedFruits[containerBoxIndex][boxIndex] = boxOfOrganizedFruits[i];
+        boxIndex += 1;
+      } 
+    }
+    System.out.println(Arrays.deepToString(boxesOfOrganizedFruits));
+    }
+}
